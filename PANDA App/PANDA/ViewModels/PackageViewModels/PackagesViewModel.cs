4ee5c4140060c89693d2938b3fecf6bc55749eb5
krum142 <krum4okkkkk@abv.bg>
2020-02-14 03:04:26 +0200

    Remove For A Contribution Test
    Test

--- PANDA App/PANDA/ViewModels/PackageViewModels/PackagesViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace PANDA.ViewModels.PackageViewModels
-{
-    public class PackagesViewModel
-    {
-        public List<ViewModels.PackageViewModels.PackageViewModels> Packages { get; set; }
-    }
-}
--- PANDA App/SIS.HTTP/StartUp.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PANDA
-{
-    public class StartUp
-    {
-        
-    }
-}
